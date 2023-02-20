3c606e783dd7d97a23d8e7037d1f7388ad31950a
Youssef <sa.youssef@fsr.ac.ma>
2023-02-20 18:16:45 +0100

    add keycloak logout

---
+++ src/main/java/com/project/controller/SecurityController.java
@@ -0,0 +1,23 @@
+package com.project.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) throws ServletException {
+		
+		request.logout();
+		return "redirect:/";
+	}
+	
+	/*
+	GetMapping("/changePassword")
+	public String cpx('')*/
+	
+}
